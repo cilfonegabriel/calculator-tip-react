cdc97b3a8c8c9fb6f5c088e6db001773b583008d
gabicilfone <cilfonegabriel@gmail.com>
2024-03-20 16:07:50 -0300

    Add principal conteiners

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,16 @@ function App() {
       <header className="bg-teal-400 py-5">
         <h1 className="text-center font-black text-4xl">Tip and Consumption Calculator</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+        </div>
+
+        <div className="">
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
