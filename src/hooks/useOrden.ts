d7033678628fcc58dc7caf372a2e2719cf556369
gabicilfone <cilfonegabriel@gmail.com>
2024-03-22 16:31:00 -0300

    import params order

--- src/hooks/useOrden.ts
+++ src/hooks/useOrden.ts
@@ -19,10 +19,9 @@ export default function useOrden() {
             setOrder([...order, newItem])
         }
     }
-
-    console.log(order)
     
     return {
+        order,
         addItem,
     }
 }
