e3b571a62653014218c401359e2ac76cfe696bd9
gabicilfone <cilfonegabriel@gmail.com>
2024-03-22 15:55:19 -0300

    Add function to avoid duplicates

--- src/hooks/useOrden.ts
+++ src/hooks/useOrden.ts
@@ -6,11 +6,21 @@ export default function useOrden() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item: MenuItem) => {
-        
-        const newItem = {...item, quantity:1}
 
-        setOrder([...order, newItem])
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if(itemExist) {
+            const updateOrder = order.map(orderItem => orderItem.id === item.id ? 
+                {...orderItem, quantity: orderItem.quantity + 1} : 
+                orderItem
+            )
+            setOrder(updateOrder)
+        } else {
+            const newItem = {...item, quantity:1}
+            setOrder([...order, newItem])
+        }
     }
+
+    console.log(order)
     
     return {
         addItem,
