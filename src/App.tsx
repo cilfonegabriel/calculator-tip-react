915d80bf522eafd6e9dfca4e66d85bac679549e9
gabicilfone <cilfonegabriel@gmail.com>
2024-03-22 16:30:30 -0300

    Import component to show order in the app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { menuItems } from "./data/db"
 import MenuItem from "./components/MenuItem"
 import useOrder from "./hooks/useOrden"
+import OrderContents from "./components/OrderContents"
 
 function App() {
 
-  const { addItem } = useOrder()
+  const { order,addItem } = useOrder()
   
   return (
     <>
@@ -29,8 +30,11 @@ function App() {
 
         </div>
 
-        <div className="">
-          <h2>Consumo</h2>
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents 
+            order = {order}
+          />
+          
         </div>
       </main>
     </>
