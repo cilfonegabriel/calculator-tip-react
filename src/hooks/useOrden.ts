5c115230b63223e5bcf30aecdd93328ffb69f972
gabicilfone <cilfonegabriel@gmail.com>
2024-03-22 15:23:50 -0300

    Add elements to state order

--- src/hooks/useOrden.ts
+++ src/hooks/useOrden.ts
@@ -6,7 +6,10 @@ export default function useOrden() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item: MenuItem) => {
-        console.log(item)
+        
+        const newItem = {...item, quantity:1}
+
+        setOrder([...order, newItem])
     }
     
     return {
