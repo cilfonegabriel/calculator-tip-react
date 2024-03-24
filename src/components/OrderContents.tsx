01855514fe37c3e3c32ec985941bf02f9978328f
gabicilfone <cilfonegabriel@gmail.com>
2024-03-24 12:53:33 -0300

    Use funtion to delete an article and add promps

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,9 +1,9 @@
 import { formatCurrency } from "../helpers"
-import { OrderItem } from "../types"
+import { MenuItem, OrderItem } from "../types"
 
 type OrderContentsProps = {
     order: OrderItem[]
-    removeItem: () => void
+    removeItem: (id: MenuItem['id']) => void
 }
 
 export default function OrderContents({order, removeItem}: OrderContentsProps) {
@@ -29,7 +29,7 @@ export default function OrderContents({order, removeItem}: OrderContentsProps) {
                             </div>
                             <button 
                                 className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
-                                onClick={() => removeItem()}
+                                onClick={() => removeItem(item.id)}
                             >
                                 X
                             </button>
