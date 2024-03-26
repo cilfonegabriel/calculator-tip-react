a8e33469f716ec83437f0435a266d854e252e26c
gabicilfone <cilfonegabriel@gmail.com>
2024-03-26 10:35:17 -0300

    Change useMemo to useCallback and add style and function to button to save an order

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,39 +1,44 @@
-import { useMemo } from "react"
+import { useCallback } from "react"
 import { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
 
 type OrderTotalProps = {
     order:OrderItem[],
     tip: number,
+    placeOrder: () => void
 }
 
-export default function OrderTotals({order, tip} : OrderTotalProps) {
+export default function OrderTotals({order, tip, placeOrder} : OrderTotalProps) {
 
-    const subtotalAmount =useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+    const subtotalAmount =useCallback(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
 
-    const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order])
+    const tipAmount = useCallback(() => subtotalAmount() * tip, [tip, order])
 
-    const totalAmount = useMemo(() => subtotalAmount + tip, [tip, order])
+    const totalAmount = useCallback(() => subtotalAmount() + tipAmount(), [tip, order])
 
   return (
     <>
         <div className="space-y-3">
             <h2 className="font-black text-2xl">Totales y Propina</h2>
             <p>Subtotal a pagar: {''}
-                <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
+                <span className="font-bold">{formatCurrency(subtotalAmount())}</span>
             </p>
 
             <p>Propina: {''}
-                <span className="font-bold">{formatCurrency(tipAmount)}</span>
+                <span className="font-bold">{formatCurrency(tipAmount())}</span>
             </p>
 
             <p>Total a pagar: {''}
-                <span className="font-bold">{formatCurrency(totalAmount)}</span>
+                <span className="font-bold">{formatCurrency(totalAmount())}</span>
             </p>
         </div>
 
-        <button>
-
+        <button
+            className="w-full bg-black p-3 mt-10 uppercase text-white font-bold disabled:opacity-10"
+            disabled = {totalAmount() === 0}
+            onClick={placeOrder}
+        >
+            Guardar Orden
         </button>
     </>
   )
